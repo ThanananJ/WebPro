1acf3e78ea2068b73af15481ddaf24a7aa03ae2a
Donnaya <donnayawang@gmail.com>
2019-11-16 23:35:58 +0700

    [UPDATE] SearchingServlet

--- src/java/quinn/servlet/SearchingServlet.java
+++ src/java/quinn/servlet/SearchingServlet.java
@@ -36,31 +36,40 @@ public class SearchingServlet extends HttpServlet {
         String quizName = request.getParameter("quizName");
         String grade = request.getParameter("grade");
         String subjects = request.getParameter("subjects");
-
+        
         QuizController qc = new QuizController();
         List<Quiz> q = null;
-
-        if (quizName.isEmpty()) {
-            qc.findByDescription(quizName, grade, subjects);
-        } else {
-            qc.findByGradeSubject(grade, subjects);
-
+        
+        if(!quizName.isEmpty()){
+            q = qc.findByDescription(quizName, grade, subjects);
+//            q = qc.findByDesc(quizName);
+            request.setAttribute("search", quizName);
+            request.setAttribute("quiz", q);
+            request.getServletContext().getRequestDispatcher("/resultQuiz.jsp").forward(request, response);
+        }else{
+            q = qc.findByGradeSubject(grade, subjects);
+            request.setAttribute("search", grade);
+            request.setAttribute("quiz", q);
+            request.getServletContext().getRequestDispatcher("/resultQuiz.jsp").forward(request, response);
         }
-    }
+        
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        
+}
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+/**
+ * Handles the HTTP <code>GET</code> method.
+ *
+ * @param request servlet request
+ * @param response servlet response
+ * @throws ServletException if a servlet-specific error occurs
+ * @throws IOException if an I/O error occurs
+ */
+@Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getServletContext().getRequestDispatcher("/resultQuiz.jsp").forward(request, response);
     }
 
     /**
@@ -72,7 +81,7 @@ public class SearchingServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -83,7 +92,7 @@ public class SearchingServlet extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-    public String getServletInfo() {
+        public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
